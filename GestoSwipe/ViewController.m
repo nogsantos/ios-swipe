fd92d5c64377ae41f838e0497c14b2d208f1e6f6
Fabricio Nogueira <nogsantos@gmail.com>
2015-09-16 16:50:38 -0300

    First commit

--- GestoSwipe/ViewController.m
+++ GestoSwipe/ViewController.m
@@ -16,12 +16,101 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self criaQuadrado];
+    [self registraGesto];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+/**
+ * Cria a view que será manipulada
+ */
+-(void) criaQuadrado{
+    
+    quadrado = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    quadrado.backgroundColor =  [UIColor yellowColor];
+    [self.view addSubview:quadrado];
+    
+}
+/**
+ * Responsável por adicionar os swipes
+ */
+-(void) registraGesto{
+    
+    [self adicionaGesto:UISwipeGestureRecognizerDirectionRight];
+    [self adicionaGesto:UISwipeGestureRecognizerDirectionLeft];
+    
+    [self adicionaGesto:UISwipeGestureRecognizerDirectionUp];
+    [self adicionaGesto:UISwipeGestureRecognizerDirectionDown];
+    
+}
+/**
+ * Realiza o trabalho de adicionar os gestos
+ */
+-(void) adicionaGesto:(UISwipeGestureRecognizerDirection) direcao{
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc]
+                                        initWithTarget : self
+                                        action         : @selector(jogaQuadrado:)
+                                       ];
+    swipe.direction                 = direcao;
+    [self.view addGestureRecognizer:swipe];
+}
+/**
+ *
+ */
+-(void) jogaQuadrado:(UIGestureRecognizer *) gesto{
+    
+    CGPoint location = [gesto locationInView:quadrado];
+    /*
+     * Verifica se o toque ocorreu na área do quadrado propriamente dito
+     */
+    if ([quadrado pointInside:location withEvent:nil]) {
+        UISwipeGestureRecognizer *swipe = (UISwipeGestureRecognizer *)gesto;
+        
+        float novoX = swipe.direction == UISwipeGestureRecognizerDirectionLeft ? 0 : (self.view.frame.size.width - quadrado.frame.size.width);
+        
+        [UIView
+            animateWithDuration : 0.3
+            delay               : 0
+            options             : UIViewAnimationOptionCurveEaseInOut
+            animations:^{
+                CGRect frame   = quadrado.frame;
+                frame.origin.x = novoX;
+                quadrado.frame = frame;
+                
+            } completion:nil
+        ];
+    }
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
